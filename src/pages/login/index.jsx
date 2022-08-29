3a7346ac23f35bbbd121ea223fd7d4fc539b8088
Vaishna K p <vaishnakp1999@gmail.com>
2022-08-29 14:49:12 +0530

    login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 import BackgroundImage from "../../assets/login_bg.jpg";
 
 import "../../styles/common.styles.scss";
 import "./login.styles.scss";
+import { Typography } from "@mui/material";
 
 const Login = () => {
   return (
@@ -15,42 +18,52 @@ const Login = () => {
         style={{ backgroundImage: `url(${BackgroundImage})` }}
       >
         <div className="formWraper">
-          <div className="formDiv">
-            <form>
-              <h2>Sign in</h2>
-              <p className="text"> New user? Create an account</p>
-              <TextField fullWidth id="standard-basic" label="Email Address" variant="standard" />
-              {/* <hr />
-              <div className="orDiv">Or</div> */}
-              <div className="checkBox">
-                <input type="checkbox" name="checkbox" id="checkbox" />
-                <span className="text">Keep me signed in</span>
-              </div>
-              <button className="btn btn-success" type="submit">
-                SIGN IN
-              </button>
-              <div className="forgotpassword-link">
-                <p>
-                  <a
-                    href="#"
-                    className="text"
-                  >
-                    Forgot Password
-                  </a>{" "}
-                </p>
-              </div>
-              <div className="signup-link">
+          <div className="welcome-section">
+            <div className="welcomeDiv">
+              <h2>Welcome Back!</h2>
+            </div>
+          </div>
+          <div className="main-div">
+            <div className="formDiv">
+              <form>
+                <h2>Sign in</h2>
                 <p className="text">
-                  Create new account?
-                  <a
-                    href="#"
-                    className="text"
-                  >
-                    Sign Up
+                  {" "}
+                  New user?
+                  <a href="" className="link-text">
+                    Create an account
                   </a>{" "}
                 </p>
-              </div>
-            </form>
+                <TextField
+                  fullWidth
+                  id="standard-basic"
+                  label="Email Address"
+                  variant="standard"
+                />
+                <div className="error">Invalid Email Address</div>
+                <div className="signin-btn">
+                  <button className="btn btn-success" type="submit">
+                    Continue
+                  </button>
+                </div>
+                <div class="separator">Or</div>
+                <div className="socialBtn">
+                  <div className="twitter icon text">
+                    <GoogleIcon className="icons-size" />
+                    Continue with Google
+                  </div>
+                  <div className="facebook icon text">
+                    <FacebookIcon className="icons-size " />
+                    Continue with Facebook
+                  </div>
+                </div>
+                <div className="back-btn">
+                  <a href="#" className="text">
+                    Back to home
+                  </a>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
