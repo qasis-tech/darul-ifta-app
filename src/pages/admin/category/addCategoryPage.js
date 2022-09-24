c5cac72f989636062b5be627fc7aed2aba1c4283
Geethu KN <geethu90@gmail.com>
2022-09-24 16:28:01 +0530

    validation in ask fatwas

--- src/pages/admin/category/addCategoryPage.js
+++ src/pages/admin/category/addCategoryPage.js
@@ -1,46 +1,96 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Button from "@mui/material/Button";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import "./addcategory.styles.scss";
 import { Chip } from "@mui/material";
+import { URLS } from "../../../config/urls.config";
 const top100Films = [{ label: "The Shawshank Redemption", year: 1994 }];
 
 export default function AddCategories() {
+  const [selectedSubcategory, setSelectedSubcategory] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    getCatgoryApi();
+  }, []);
+
+  const getCatgoryApi = () => {
+    axios
+      .get(URLS.category, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log("res category", res);
+      })
+      .catch((err) => {
+        console.log("error category", err);
+      });
+  };
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdlZXRodTkwQGdtYWlsLmNvbSIsImlhdCI6MTY2NDAwMTE4NywiZXhwIjoxNjg5OTIxMTg3fQ.5wiCZurHaz4BmYPaQ67Hf3zFMInWcOdSCyUzYo-4YWQ";
+  const handleCreate = ({ category, subCategory }) => {
+    let payload = { category: category, subCategory: selectedSubcategory };
+    axios
+      .post(`${URLS.category}`, payload, {
+        headers: {
+          Authorization: `${token}`,
+          "content-type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log("res category", res);
+      })
+      .catch((err) => {
+        console.log("Error in Category Add", err);
+      });
+  };
+  const handlesubCategory = (e, val) => setSelectedSubcategory(val);
+
   return (
     <div className="add-category-section">
-      <div className="add-category-container">
-        <div className="add-category-row">
-          <div className="col-md-12">
-            <Autocomplete
-              id="tags-filled"
-              options={top100Films.map((option) => option.label)}
-              freeSolo
-              size="small"
-              renderTags={(value, getTagProps) =>
-                value.map((option, index) => (
-                  <Chip
+      <form onSubmit={handleSubmit(handleCreate)}>
+        <div className="add-category-container">
+          <div className="add-category-row">
+            <div className="col-md-12">
+              <Autocomplete
+                id="tags-filled"
+                options={top100Films.map((option) => option.label)}
+                freeSolo
+                size="small"
+                renderTags={(value, getTagProps) =>
+                  value.map((option, index) => (
+                    <Chip
+                      variant="outlined"
+                      label={option}
+                      size="small"
+                      {...getTagProps({ index })}
+                    />
+                  ))
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
                     variant="outlined"
-                    label={option}
+                    label="Category"
+                    placeholder="Category"
                     size="small"
-                    {...getTagProps({ index })}
+                    {...register("category")}
                   />
-                ))
-              }
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant="outlined"
-                  label="Category"
-                  placeholder="Category"
-                  size="small"
-                />
-              )}
-            />
-          </div>
-          <div className="col-md-12 subcategory">
-            {/* <Autocomplete
+                )}
+              />
+            </div>
+            <div className="col-md-12 subcategory">
+              {/* <Autocomplete
               disablePortal
               size="small"
               id="combo-box-demo"
@@ -49,42 +99,51 @@ export default function AddCategories() {
                 <TextField {...params} label="Subcategory" />
               )}
             /> */}
-            <Autocomplete
-              multiple
-              id="tags-filled"
-              options={top100Films.map((option) => option.label)}
-              freeSolo
-              size="small"
-              renderTags={(value, getTagProps) =>
-                value.map((option, index) => (
-                  <Chip
+              <Autocomplete
+                multiple
+                id="tags-filled"
+                options={top100Films.map((option) => option.label)}
+                freeSolo
+                value={selectedSubcategory}
+                onChange={(e, val) => handlesubCategory(e, val)}
+                size="small"
+                renderTags={(value, getTagProps) =>
+                  value.map((option, index) => (
+                    <Chip
+                      variant="outlined"
+                      label={option}
+                      size="small"
+                      {...getTagProps({ index })}
+                    />
+                  ))
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
                     variant="outlined"
-                    label={option}
+                    label="Subcategory"
+                    placeholder="Subcategory"
                     size="small"
-                    {...getTagProps({ index })}
+                    {...register("subCategory")}
                   />
-                ))
-              }
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant="outlined"
-                  label="Subcategory"
-                  placeholder="Subcategory"
-                  size="small"
-                />
-              )}
-            />
+                )}
+              />
+            </div>
+          </div>
+          <div className="btn-row">
+            <div className="col-md-1">
+              <Button
+                type="submit"
+                variant="contained"
+                className="form-btn"
+                fullWidth
+              >
+                CREATE
+              </Button>
+            </div>
           </div>
         </div>
-        <div className="btn-row">
-          <div className="col-md-1">
-            <Button variant="contained" className="form-btn" fullWidth>
-              CREATE
-            </Button>
-          </div>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/pages/user/Accounts/askFatwas/index.jsx
+++ src/pages/user/Accounts/askFatwas/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { URLS } from "../../../../config/urls.config";
 import { TextField, Autocomplete, Button } from "@mui/material";
 
 import "./askfatwas.styles.scss";
@@ -8,94 +10,150 @@ export default function AskFatwasComponent() {
   const [madhabList, setMadhabList] = useState([]);
   const [categoryList, setCategoryList] = useState([]);
   const [languageList, setLanguageList] = useState([]);
+  const [categoryData, setCategoryData] = useState([]);
+  const [madhabData, setMadhabData] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getCatgoryApi();
+    getmadhabApi();
+  }, []);
+
+  const getCatgoryApi = () => {
+    axios
+      .get(URLS.category, {
+        headers: {
+          // Authorization: `${token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log("res category11===>>", res.data.data);
+        setCategoryData(res.data.data);
+      })
+      .catch((err) => {
+        console.log("error category", err);
+      });
+  };
+
+  const getmadhabApi = () => {
+    axios
+      .get(URLS.madhab, {
+        headers: {
+          // Authorization: `${token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log("res mathab===>>.", res.data.data);
+        setMadhabData(res.data.data);
+      })
+      .catch((err) => {
+        console.log("error madhab", err);
+      });
+  };
+
+  const handleSubmitQuestion = (data) => {
+    console.log("data", data);
+  };
 
   return (
-    <div className="form-section">
-      <div className="form-container">
-        <div className="row">
-          <div className="col-md-4">
-            {/* <TextField
-              id="outlined-basic"
-              size="small"
-              fullWidth
-              label="Madhab"
-              variant="outlined"
-            /> */}
-
-            <Autocomplete
-              id="outlined-basic"
-              size="small"
-              options={madhabList}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="
+    <form onSubmit={handleSubmit(handleSubmitQuestion)}>
+      <div className="form-section">
+        <div className="form-container">
+          <div className="row">
+            <div className="col-md-4">
+              <Autocomplete
+                id="outlined-basic"
+                size="small"
+                options={madhabList}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="
                   Madhab"
-                />
-              )}
-            />
-          </div>
-          <div className="col-md-4">
-            <Autocomplete
-              id="combo-box-demo"
-              size="small"
-              options={categoryList}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="
-              Category"
-                />
-              )}
-            />
-          </div>
-          <div className="col-md-4">
-            <Autocomplete
-              id="combo-box-demo"
-              size="small"
-              options={languageList}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="
+                    {...register("madhab", { required: "This is required" })}
+                  />
+                )}
+              />
+
+              <div className="error">{errors?.madhab?.message}</div>
+            </div>
+            <div className="col-md-4">
+              <Autocomplete
+                id="combo-box-demo"
+                size="small"
+                options={categoryList}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="
+                Category"
+                    {...register("category", {
+                      required: "This is required",
+                    })}
+                  />
+                )}
+              />
+
+              <div className="error">{errors?.category?.message}</div>
+            </div>
+            <div className="col-md-4">
+              <Autocomplete
+                id="combo-box-demo"
+                size="small"
+                options={languageList}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="
               Language"
-                />
-              )}
-            />
+                    {...register("language", { required: "This is required" })}
+                  />
+                )}
+              />
+              <div className="error">{errors?.language?.message}</div>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <TextField
-              id="outlined-basic"
-              size="small"
-              fullWidth
-              label="Short Question"
-              variant="outlined"
-            />
+          <div className="row">
+            <div className="col-md-12">
+              <TextField
+                id="outlined-basic"
+                size="small"
+                fullWidth
+                label="Short Question"
+                variant="outlined"
+                {...register("shortQuestion", { required: "This is required" })}
+              />
+              <div className="error">{errors?.shortQuestion?.message}</div>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <TextField
-              id="outlined-multiline-static"
-              label="Question "
-              multiline
-              fullWidth
-              rows={4}
-            />
+          <div className="row">
+            <div className="col-md-12">
+              <TextField
+                id="outlined-multiline-static"
+                label="Question "
+                multiline
+                fullWidth
+                rows={4}
+                {...register("question", { required: "This is required" })}
+              />
+              <div className="error">{errors?.question?.message}</div>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className=" btn-section">
-            <Button className="submit-btn" variant="contained">
-              Submit Question
-            </Button>
+          <div className="row">
+            <div className=" btn-section">
+              <Button type="submit" className="submit-btn" variant="contained">
+                Submit Question
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
