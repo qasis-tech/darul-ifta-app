f41119376eafe67a09dc46feb2696a88763d7713
Geethu KN <geethu90@gmail.com>
2022-10-01 17:46:10 +0530

    fatwa filter

--- src/pages/admin/fatwas/index.jsx
+++ src/pages/admin/fatwas/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
@@ -21,16 +22,47 @@ import axios from "axios";
 import { URLS } from "../../../config/urls.config";
 import NoDataAvailable from "../../../components/NoDataAvailable";
 import { formatDate } from "../../../utils/dateformat";
-import { set } from "react-hook-form";
+
 import Loader from "../../../components/common/Loader";
 const top100Films = [{ label: "The Shawshank Redemption", year: 1994 }];
 
 export default function Fatwas() {
   const [questionList, setQuestionList] = useState([]);
   const [isLoader, setLoader] = useState(false);
+  const [madhabData, setMadhabData] = useState([]);
+  const [selectedMadhab, setSelectedMadhab] = useState([]);
+  const [status, setStatus] = useState([
+    { id: 1, title: "Pending" },
+    { id: 2, title: "Rejected" },
+    { id: 3, title: "Re Submitted" },
+    { id: 4, title: "Received to Darul Ifta" },
+    { id: 5, title: "Assigned Mufti" },
+    { id: 6, title: "Mufti Answered" },
+    { id: 7, title: "Completed Verification" },
+    { id: 8, title: "Published" },
+  ]);
+  const [selectedStatus, setSelectedStatus] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState([]);
+  const [selectedSubCategory, setSelectedSubCategory] = useState([]);
+  const [languageList, setLanguageList] = useState([
+    { id: 1, title: "English" },
+    { id: 2, title: "Malayalam" },
+    { id: 3, title: "Arabic" },
+    { id: 4, title: "Urdu" },
+  ]);
+  const [selectedLanguage, setSelectedLanguage] = useState([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     getQuestionListApi();
+    getmadhabApi();
+    getCatgoryApi();
   }, []);
 
   const getQuestionListApi = () => {
@@ -41,10 +73,10 @@ export default function Fatwas() {
           "Content-Type": "application/json",
         },
       })
-      .then(({ data }) => {
+      .then((res) => {
         setLoader(false);
-        console.log("res question===>>", data.data);
-        setQuestionList(data.data);
+        console.log("res question===>>", res);
+        // setQuestionList(data.data);
       })
       .catch((err) => {
         setLoader(false);
@@ -52,92 +84,210 @@ export default function Fatwas() {
       });
   };
 
+  const getmadhabApi = () => {
+    setLoader(true);
+    axios
+      .get(URLS.madhab, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        setLoader(false);
+        console.log("res madhabb1111==>", res.data);
+        setMadhabData(res.data.data);
+      })
+      .catch((err) => {
+        setLoader(false);
+        console.log("error madhab", err);
+        setMadhabData([]);
+      });
+  };
+
+  const getCatgoryApi = () => {
+    axios
+      .get(URLS.category, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(({ data }) => {
+        console.log("res category", data.data);
+        setCategoryList(data.data);
+      })
+      .catch((err) => {
+        console.log("error category", err);
+      });
+  };
+
+  const handleApply = () => {};
+
   const navigate = useNavigate();
 
   return (
     <div className="admin-fatwas-section">
-      <div className="fatwas-container">
-        <div className="fatwas-row">
-          <div className="col-md-2">
-            <Autocomplete
-              disablePortal
-              size="small"
-              id="combo-box-demo"
-              options={top100Films}
-              renderInput={(params) => <TextField {...params} label="Status" />}
-            />
+      <form onSubmit={handleSubmit(handleApply)}>
+        <div className="fatwas-container">
+          <div className="fatwas-row">
+            <div className="col-md-2">
+              {status?.length ? (
+                <Autocomplete
+                  disablePortal
+                  id="combo-box-demo"
+                  size="small"
+                  options={status}
+                  getOptionLabel={(option) => option.title || ""}
+                  isOptionEqualToValue={(option, value) =>
+                    option.id === value.id
+                  }
+                  onChange={(e, val) => setSelectedStatus(val)}
+                  value={selectedStatus}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="
+                      Active Status"
+                      {...register("status")}
+                    />
+                  )}
+                />
+              ) : null}
+            </div>
+            <div className="col-md-2">
+              {madhabData?.length ? (
+                <Autocomplete
+                  disablePortal
+                  id="combo-box-demo"
+                  size="small"
+                  options={madhabData}
+                  getOptionLabel={(option) => option.title || ""}
+                  isOptionEqualToValue={(option, value) =>
+                    option._id === value._id
+                  }
+                  onChange={(e, val) => setSelectedMadhab(val)}
+                  value={selectedMadhab}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="
+                    Madhab"
+                      {...register("madhab")}
+                    />
+                  )}
+                />
+              ) : null}
+            </div>
+            <div className="col-md-2">
+              {categoryList?.length ? (
+                <Autocomplete
+                  disablePortal
+                  id="combo-box-demo"
+                  size="small"
+                  options={categoryList}
+                  getOptionLabel={(option) => option.category || ""}
+                  isOptionEqualToValue={(option, value) =>
+                    option._id === value._id
+                  }
+                  onChange={(e, val) => setSelectedCategory(val)}
+                  value={selectedCategory}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="
+                    Category"
+                      {...register("category")}
+                    />
+                  )}
+                />
+              ) : null}
+            </div>
+            <div className="col-md-2">
+              <Autocomplete
+                disablePortal
+                size="small"
+                id="combo-box-demo"
+                options={
+                  selectedCategory?.subCategory?.length
+                    ? selectedCategory?.subCategory
+                    : []
+                }
+                getOptionLabel={(option) => option.label || ""}
+                isOptionEqualToValue={(option, value) =>
+                  option._id === value._id
+                }
+                onChange={(e, val) => setSelectedSubCategory(val)}
+                value={selectedSubCategory}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Subcategories"
+                    size="small"
+                    {...register("subCategory")}
+                  />
+                )}
+              />
+            </div>
           </div>
-          <div className="col-md-2">
-            <Autocomplete
-              disablePortal
-              size="small"
-              id="combo-box-demo"
-              options={top100Films}
-              renderInput={(params) => <TextField {...params} label="Madhab" />}
-            />
-          </div>
-          <div className="col-md-2">
-            <Autocomplete
-              disablePortal
-              size="small"
-              id="combo-box-demo"
-              options={top100Films}
-              renderInput={(params) => (
-                <TextField {...params} label="Category" />
+          <div className="fatwas-row second-row">
+            <div className="col-md-2">
+              <Autocomplete
+                disablePortal
+                size="small"
+                id="combo-box-demo"
+                options={top100Films}
+                renderInput={(params) => (
+                  <TextField {...params} label="Mufti" />
+                )}
+              />
+            </div>
+            <div className="col-md-2">
+              <Autocomplete
+                disablePortal
+                size="small"
+                id="combo-box-demo"
+                options={top100Films}
+                renderInput={(params) => (
+                  <TextField {...params} label="Mustafthi" />
+                )}
+              />
+            </div>
+            <div className="col-md-2">
+              {languageList?.length && (
+                <Autocomplete
+                  disablePortal
+                  id="combo-box-demo"
+                  size="small"
+                  options={languageList}
+                  getOptionLabel={(option) => option.title || ""}
+                  isOptionEqualToValue={(option, value) =>
+                    option.id === value.id
+                  }
+                  onChange={(e, val) => setSelectedLanguage(val)}
+                  value={selectedLanguage}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="
+              Language"
+                      {...register("language")}
+                    />
+                  )}
+                />
               )}
-            />
-          </div>
-          <div className="col-md-2">
-            <Autocomplete
-              disablePortal
-              size="small"
-              id="combo-box-demo"
-              options={top100Films}
-              renderInput={(params) => (
-                <TextField {...params} label="Subcategory" />
-              )}
-            />
+            </div>
+            <div className="col-md-2">
+              <Button
+                type="submit"
+                variant="contained"
+                className="form-btn"
+                fullWidth
+              >
+                APPLY
+              </Button>
+            </div>
           </div>
         </div>
-        <div className="fatwas-row second-row">
-          <div className="col-md-2">
-            <Autocomplete
-              disablePortal
-              size="small"
-              id="combo-box-demo"
-              options={top100Films}
-              renderInput={(params) => <TextField {...params} label="Mufti" />}
-            />
-          </div>
-          <div className="col-md-2">
-            <Autocomplete
-              disablePortal
-              size="small"
-              id="combo-box-demo"
-              options={top100Films}
-              renderInput={(params) => (
-                <TextField {...params} label="Mustafthi" />
-              )}
-            />
-          </div>
-          <div className="col-md-2">
-            <Autocomplete
-              disablePortal
-              size="small"
-              id="combo-box-demo"
-              options={top100Films}
-              renderInput={(params) => (
-                <TextField {...params} label="Language" />
-              )}
-            />
-          </div>
-          <div className="col-md-2">
-            <Button variant="contained" className="form-btn" fullWidth>
-              APPLY
-            </Button>
-          </div>
-        </div>
-      </div>
+      </form>
       <hr />
       <div className="table-section">
         <div className="table-row">
