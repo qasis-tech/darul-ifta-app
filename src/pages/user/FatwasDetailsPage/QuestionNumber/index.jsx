fbbe87f8d542c750e7009a65d267c2f45cdd061b
Sabeer ali <sabeer@sectorqube.com>
2022-10-22 09:56:03 +0530

    question details

--- src/pages/user/FatwasDetailsPage/QuestionNumber/index.jsx
+++ src/pages/user/FatwasDetailsPage/QuestionNumber/index.jsx
@@ -1,23 +1,36 @@
+import { Chip } from "@mui/material";
 import React from "react";
 import "./question.number.styles.scss";
 
 export default function QuestionNumber({ data }) {
-  const tempSubCategory = data?.sub_category?.map((subcategory) => {
-    return subcategory.label;
-  });
+  const tempSubCategory = data?.sub_category?.map(
+    (subcategory) => subcategory.label
+  );
 
   return (
     <div className="question-number-section">
       <div className="col-md-12 py-1 qid">
         <h6>
-          Question ID : QID-{data?.slNo.toString().padStart(3, "0") || "N/A"} /
-          Madhab : {data?.madhab?.title || "N/A"} / SubCategory :
+          Question ID :
+          <Chip
+            label={`Q${data?.slNo?.toString()?.padStart(3, "0")}`}
+            className="single-chip"
+            style={{ marginLeft: 15 }}
+          />
+          Madhab :
+          <Chip
+            label={data?.madhab?.title}
+            className="single-chip"
+            style={{ marginLeft: 10 }}
+          />
+          SubCategory :
           {tempSubCategory?.map((item, index) => {
             return (
-              <span>
-                {item}
-                &nbsp;&nbsp;&nbsp;
-              </span>
+              <Chip
+                label={item}
+                className="single-chip"
+                style={{ marginLeft: 10 }}
+              />
             );
           })}
         </h6>
