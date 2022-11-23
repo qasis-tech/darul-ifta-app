c4d6e9733894940a628fe9e135db8f0a19ca4a99
Sabeer Ali <qasistechnologies@gmail.com>
2022-11-23 13:40:31 +0530

    admin-fatwas

--- src/components/FatwaAddComponent.jsx
+++ src/components/FatwaAddComponent.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import { Button, TextField } from "@mui/material";
 
 import AddIcon from "@mui/icons-material/Add";
-import CloseIcon from "@mui/icons-material/Close";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
 import "../styles/fatwa.add.styles.scss";
@@ -14,7 +13,7 @@ export default function FatwaAddComponent({ referenceList, setReferance }) {
     if (referenceList?.length) {
       setList(referenceList);
     }
-  }, []);
+  }, [referenceList]);
 
   const [list, setList] = useState([
     {
@@ -44,7 +43,6 @@ export default function FatwaAddComponent({ referenceList, setReferance }) {
   };
 
   const handleData = (val, field, index) => {
-    console.log("val, field", val, field);
     let temp = [...list];
     temp[index][`${field}`] = val;
     setList(temp);
@@ -68,69 +66,74 @@ export default function FatwaAddComponent({ referenceList, setReferance }) {
           </div>
         </div>
       </div>
-      {list?.map((item, index) => {
-        return (
-          <div key={index} className="show-table-container">
-            <div className="col-md-10">
-              <div className="show-table-row">
-                <div className="col-md-12">
-                  <TextField
-                    id="quote"
-                    label="Quote"
-                    size="small"
-                    fullWidth
-                    value={item.quote}
-                    onChange={(e) => handleData(e.target.value, "quote", index)}
-                  />
-                </div>
-                <div className="second-row">
-                  <div className="col-md-4 first-col">
+      {list?.length &&
+        list?.map((item, index) => {
+          return (
+            <div key={index} className="show-table-container">
+              <div className="col-md-10">
+                <div className="show-table-row">
+                  <div className="col-md-12">
                     <TextField
-                      id="bookName"
-                      label="Book Name"
+                      id="quote"
+                      label="Quote"
                       size="small"
                       fullWidth
-                      value={item.bookName}
+                      value={item.quote}
                       onChange={(e) =>
-                        handleData(e.target.value, "bookName", index)
+                        handleData(e.target.value, "quote", index)
                       }
                     />
                   </div>
-                  <div className="col-md-4">
-                    <TextField
-                      id="volume"
-                      label="Volume"
-                      size="small"
-                      fullWidth
-                      value={item.vol}
-                      onChange={(e) => handleData(e.target.value, "vol", index)}
-                    />
-                  </div>
-                  <div className="col-md-4 second-col">
-                    <TextField
-                      id="pgNo"
-                      label="Page Number"
-                      size="small"
-                      fullWidth
-                      value={item.pgNo}
-                      onChange={(e) =>
-                        handleData(e.target.value, "pgNo", index)
-                      }
-                    />
+                  <div className="second-row">
+                    <div className="col-md-4 first-col">
+                      <TextField
+                        id="bookName"
+                        label="Book Name"
+                        size="small"
+                        fullWidth
+                        value={item.bookName}
+                        onChange={(e) =>
+                          handleData(e.target.value, "bookName", index)
+                        }
+                      />
+                    </div>
+                    <div className="col-md-4">
+                      <TextField
+                        id="volume"
+                        label="Volume"
+                        size="small"
+                        fullWidth
+                        value={item.vol}
+                        onChange={(e) =>
+                          handleData(e.target.value, "vol", index)
+                        }
+                      />
+                    </div>
+                    <div className="col-md-4 second-col">
+                      <TextField
+                        id="pgNo"
+                        label="Page Number"
+                        size="small"
+                        fullWidth
+                        value={item.pgNo}
+                        onChange={(e) =>
+                          handleData(e.target.value, "pgNo", index)
+                        }
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
+              <div className="col-md-2 close-section">
+                {list?.length > 1 && (
+                  <div className="close-button">
+                    <HighlightOffIcon onClick={() => remove(index)} />
+                  </div>
+                )}
+              </div>
             </div>
-            <div className="col-md-2 close-section">
-              {list?.length > 1 && (
-                <div className="close-button">
-                  <HighlightOffIcon onClick={() => remove(index)} />
-                </div>
-              )}
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
       <hr />
     </div>
   );
