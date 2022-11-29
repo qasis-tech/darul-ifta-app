fb309a2df82c3fb7091405b011fb9650881cf8ca
Vaishna <vaishnakp1999@gmail.com>
2022-11-29 09:50:18 +0530

    fixed footer social links and question container radius

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -100,19 +100,19 @@ const FooterComponent = () => {
                 <div>
                   <h4 className="fw-bold">Follow Us</h4>
                   <div className="">
-                    <a href="https://www.facebook.com/Kauzariyya">
+                    <a href="https://www.facebook.com/Kauzariyya" target="_blank">
                       <img src={FbPic} alt="" />
                     </a>
-                    <a href="https://instagram.com/kauzariyya">
+                    <a href="https://instagram.com/kauzariyya" target="_blank">
                       <img src={InstaPic} alt="" />
                     </a>
-                    <a href="https://twitter.com/kauzariyya">
+                    <a href="https://twitter.com/kauzariyya" target="_blank">
                       <img src={TwitterPic} alt="" />
                     </a>
-                    <a href="https://wa.me/919633712592?text=%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%20%D8%B9%D9%84%D9%8A%D9%83%D9%85%20%D9%88%D8%B1%D8%AD%D9%85%D8%A9%20%D8%A7%D9%84%D9%84%D9%87%20%D9%88%D8%A8%D8%B1%D9%83%D8%A7%D8%AA%D9%87%0AI%20need%20your%20help%20asking%20for%20a%20new%20fatwa%20%0Ain%20DARUL%20IFTA%20KAUZARIYYA%20%0AMy%20Name%20is%20;%20">
+                    <a href="https://wa.me/919633712592?text=%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%20%D8%B9%D9%84%D9%8A%D9%83%D9%85%20%D9%88%D8%B1%D8%AD%D9%85%D8%A9%20%D8%A7%D9%84%D9%84%D9%87%20%D9%88%D8%A8%D8%B1%D9%83%D8%A7%D8%AA%D9%87%0AI%20need%20your%20help%20asking%20for%20a%20new%20fatwa%20%0Ain%20DARUL%20IFTA%20KAUZARIYYA%20%0AMy%20Name%20is%20;%20" target="_blank">
                       <img src={WhatsappPic} alt="" />
                     </a>
-                    <a href="https://youtube.com/@Kauzariyya">
+                    <a href="https://youtube.com/@Kauzariyya" target="_blank">
                       <img src={YoutubePic} alt="" />
                     </a>
                   </div>
